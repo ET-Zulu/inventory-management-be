cfe7d0b3cab42d42026d7fb08f6f89f66bd14eb1
mahlet <mahlet2ambaye@gmail.com>
2026-05-29 20:34:15 +0100

    fix deployment issue

--- app/start.py
+++ app/start.py
@@ -19,7 +19,7 @@ def run_migrations() -> None:
 def main() -> None:
     run_migrations()
     port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
